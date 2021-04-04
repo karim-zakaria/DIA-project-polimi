0e12c6c775400988ebf3eeedd2a8621948bb60b8
Damzwan <damian.vlaicu@gmail.com>
2021-04-04 19:25:27 +0200

    added sliding windows UCB algorithm and cleared some existing errors

--- pricing/learners/thompson.py
+++ pricing/learners/thompson.py
@@ -1,38 +1,36 @@
-
-from Learner import *
+from learner import Learner
 import numpy as np
 
-class Thomspon(Learner):
 
-  def __init__(self,n_arms):
-    super().__init__(n_arms)
-    self.beta_parameters=np.ones(n_arms,2)
+class Thompson(Learner):
 
-  def pull_arm(self):
-    idx=np.argmax(np.random.beta(self.beta_parameters[:,0],self.beta_parameters[:,1]))
-    return idx
+    def __init__(self, n_arms):
+        super().__init__(n_arms)
+        self.beta_parameters = np.ones(n_arms, 2)
 
-  def update(self, pulled_arm, reward):
-    self.t+=1
-    self.update_observations(pulled_arm, reward)
-    self.beta_parameters[pulled_arm,0]=self.beta_parameters[pulled_arm,0]+reward
-    self.beta_parameters[pulled_arm,1]=self.beta_parameters[pulled_arm,1]+1-reward
+    def pull_arm(self):
+        idx = np.argmax(np.random.beta(self.beta_parameters[:, 0], self.beta_parameters[:, 1]))
+        return idx
 
+    def update(self, pulled_arm, reward):
+        self.t += 1
+        self.update_observations(pulled_arm, reward)
+        self.beta_parameters[pulled_arm, 0] = self.beta_parameters[pulled_arm, 0] + reward
+        self.beta_parameters[pulled_arm, 1] = self.beta_parameters[pulled_arm, 1] + 1 - reward
 
 
 class SW_Thompson(Thompson):
-  def __init__(self, n_arms, window_size):
-    super().__init__(n_arms)
-    self.window_size=window_size
-    self.pulled_arms=np.array([])
+    def __init__(self, n_arms, window_size):
+        super().__init__(n_arms)
+        self.window_size = window_size
+        self.pulled_arms = np.array([])
 
-  def update(self, pulled_arm, reward):
-    self.t+=1
-    self.update_observations(pulled_arm, reward)
-    self.pulled_arms= np.append(self.pulled_arms, pulled_arm)
-    for arm in range(self.n_arms):
-      n_samples=np.sum(self.pulled_arms[-self.window_size:]==arm)
-      cum_rew=np.sum(self.rewards_per_arm[arm][-n_samples:]) if n_samples>0 else 0
-      self.beta_parameters[arm,0]=cum_rew+1.0
-      self.beta_parameters[arm,1]=n_samples-cum_rew+1
-
+    def update(self, pulled_arm, reward):
+        self.t += 1
+        self.update_observations(pulled_arm, reward)
+        self.pulled_arms = np.append(self.pulled_arms, pulled_arm)
+        for arm in range(self.n_arms):
+            n_samples = np.sum(self.pulled_arms[-self.window_size:] == arm)
+            cum_rew = np.sum(self.rewards_per_arm[arm][-n_samples:]) if n_samples > 0 else 0
+            self.beta_parameters[arm, 0] = cum_rew + 1.0
+            self.beta_parameters[arm, 1] = n_samples - cum_rew + 1
--- pricing/learners/ucb.py
+++ pricing/learners/ucb.py
@@ -1,90 +1,115 @@
-
-from Learner import Learner
+from learner import Learner
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 from matching.cumsum import CUMSUM
 
+
 class UCB(Learner):
-  def __init__(self, n_arms)
-    super().__init__(n_arms)
-    self.empirical_means=np.zeros(n_arms)
-    self.confidence=np.array([np.inf]*n_arms)
+    def __init__(self, n_arms):
+        super().__init__(n_arms)
+        self.empirical_means = np.zeros(n_arms)
+        self.confidence = np.array([np.inf] * n_arms)
 
-  def pull_arm(self):
-    upper_conf=self.empirical_means+self.confidence
-    return np.random.choice(np.where(upper_conf==upper_conf.max())[0])
+    def pull_arm(self):
+        return np.argmax(self.empirical_means + self.confidence)
 
-  def update(self, pull_arm, reward):
-      self.t+=1
-      self.empirical_means[pull_arm]=(self.empirical_means[pull_arm]*(self.t-1)+reward)/self.t
-      for a in range(self.n_arms):
-        n_samples=len(self.rewards_per_arm[a])
-        self.confidence[a]=(2*np.log(self.t)/n_samples)**0.5 if n_samples>0 else np.inf
-      self.update_observations(pull_arm, reward)
+    def update(self, pull_arm, reward):
+        self.t += 1
+        self.empirical_means[pull_arm] = (self.empirical_means[pull_arm] * (self.t - 1) + reward) / self.t
+        for a in range(self.n_arms):
+            n_samples = len(self.rewards_per_arm[a])
+            self.confidence[a] = (2 * np.log(self.t) / n_samples) ** 0.5 if n_samples > 0 else np.inf
+        self.update_observations(pull_arm, reward)
+
+
+class SW_UCB(UCB):
+    def __init__(self, n_arms, window_size):
+        super(SW_UCB, self).__init__(n_arms)
+        self.window_size = window_size
+
+    def update(self, pull_arm, reward):
+        self.t += 1
+        self.empirical_means[pull_arm] = (self.empirical_means[pull_arm] * (self.t - 1) + reward) / self.t
+        for a in range(self.n_arms):
+            self.clear_outside_window_rewards(a)
+            n_samples = len(self.rewards_per_arm[a])
+            self.confidence[a] = (2 * np.log(self.t) / n_samples) ** 0.5 if n_samples > 0 else np.inf
+        self.update_observations(pull_arm, reward)
+
+    # Remove rewards that are outside of the window for a given arm. Since the array is ordered by the timestamp and
+    # we execute this function every round, we only need to check the timestamp of the first element
+    def clear_outside_window_rewards(self, arm):
+        first_elem_timestamp = self.rewards_per_arm[arm][0][1]
+        if first_elem_timestamp < (self.t - self.window_size):
+            self.rewards_per_arm[arm].pop()
+
+    def update_observations(self, pulled_arm, reward):
+        self.rewards_per_arm[pulled_arm].append((reward, self.t))  # add a timestamp when an arm has been pulled
+        self.collected_rewards = np.append(self.collected_rewards, reward)
 
 
 class Matching_UCB(UCB):
-  def __init__(self,n_arms,n_rows,n_cols):
-    super().__init__(n_arms)
-    self.n_rows=n_rows
-    self.n_cols=n_cols
-    assert n_arms==n_cols*n_rows
+    def __init__(self, n_arms, n_rows, n_cols):
+        super().__init__(n_arms)
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        assert n_arms == n_cols * n_rows
 
-  def pull_arms(self):
-    upper_conf=self.empirical_means+self.confidence
-    upper_conf[np.isinf(upper_conf)]=1e3
-    row_ind, col_ind=linear_sum_assignment(-upper_conf.reshape(self.n_rows, self.n_cols))
-    return (row_ind, col_ind)
+    def pull_arms(self):
+        upper_conf = self.empirical_means + self.confidence
+        upper_conf[np.isinf(upper_conf)] = 1e3
+        row_ind, col_ind = linear_sum_assignment(-upper_conf.reshape(self.n_rows, self.n_cols))
+        return row_ind, col_ind
 
-  def update(self, pulled_arms, rewards):
-    self.t+=1
-    pulled_arms_flat= np.ravel_multi_index(pulled_arms,(self.n_rows, self.n_cols))
-    
-    for pulled_arm, reward in zip(pulled_arms_flat, rewards):
-      self.update_observations(pulled_arm, reward)
-      self.empirical_means[pulled_arm]=(self.empirical_means[pulled_arm]*(self.t-1)+reward)/self.t
+    def update(self, pulled_arms, rewards):
+        self.t += 1
+        pulled_arms_flat = np.ravel_multi_index(pulled_arms, (self.n_rows, self.n_cols))
 
-    for a in range(self.n_arms):
-      n_samples=len(self.rewards_per_arm[a])
-      self.confidence[a]=(2*np.log(self.t)/n_samples)**0.5 if n_samples>0 else np.inf
+        for pulled_arm, reward in zip(pulled_arms_flat, rewards):
+            self.update_observations(pulled_arm, reward)
+            self.empirical_means[pulled_arm] = (self.empirical_means[pulled_arm] * (self.t - 1) + reward) / self.t
+
+        for a in range(self.n_arms):
+            n_samples = len(self.rewards_per_arm[a])
+            self.confidence[a] = (2 * np.log(self.t) / n_samples) ** 0.5 if n_samples > 0 else np.inf
 
 
 class CUMSUM_Matching_UCB(Matching_UCB):
-  def __init__(self, n_arms, n_rows, n_cols, M=100, eps=0.05, h=20, alpha=0.01):
-    super().__init__(n_arms, n_rows, n_cols)
-    self.change_detection=[CUMSUM(M, eps, h) for _ in range(n_arms)]
-    self.valid_rewards_per_arms=[[] for _ in range(n_arms)]
-    self.detections= [[] for _ in range(n_arms)]
-    self.alpha=alpha
+    def __init__(self, n_arms, n_rows, n_cols, M=100, eps=0.05, h=20, alpha=0.01):
+        super().__init__(n_arms, n_rows, n_cols)
+        self.change_detection = [CUMSUM(M, eps, h) for _ in range(n_arms)]
+        self.valid_rewards_per_arms = [[] for _ in range(n_arms)]
+        self.detections = [[] for _ in range(n_arms)]
+        self.alpha = alpha
 
-  def pull_arm(self):
-    if np.random.binomial(1,1-self.alpha):
-      upper_conf=self.empirical_means+self.condfidence
-      upper_conf[np.isinf(upper_conf)]=1e3
-      row_ind, col_ind= linear_sum_assignment(-upper_conf.reshape(self.n_rows, self.n_cols))
-      return row_ind, col_ind
-    else:
-      costs_random= np.random.randint(0, 10, size-(self.n_rows, self.n_cols))
-      return linear_sum_assignment(costs_random)
+    def pull_arm(self):
+        if np.random.binomial(1, 1 - self.alpha):
+            upper_conf = self.empirical_means + self.condfidence
+            upper_conf[np.isinf(upper_conf)] = 1e3
+            row_ind, col_ind = linear_sum_assignment(-upper_conf.reshape(self.n_rows, self.n_cols))
+            return row_ind, col_ind
+        else:
+            costs_random = np.random.randint(0, 10, size - (self.n_rows, self.n_cols))
+            return linear_sum_assignment(costs_random)
 
-  def update(self, pulled_arms, rewards):
-    self.t+=1
-    pulled_arms_flat= np.ravel_multi_index(pulled_arms,(self.n_rows, self.n_cols))
+    def update(self, pulled_arms, rewards):
+        self.t += 1
+        pulled_arms_flat = np.ravel_multi_index(pulled_arms, (self.n_rows, self.n_cols))
 
-    for pulled_arm, reward in zip(pulled_arms_flat, rewards):
-      if self.change_detection[pulled_arm].update(reward):
-        self.detections[pulled_arm].append(self.t)
-        self.valid_rewards_per_arms[pulled_arm]=[]
-        self.change_detection[pulled_arm].reset()
-      self.update_observations(pulled_arm, reward)
-      self.empirical_means[pulled_arm]=np.mean(self.valid_rewards_per_arms[pulled_arm])
-    total_valid_samples=sum([len(x) for x in self.valid_rewards_per_arm])
+        for pulled_arm, reward in zip(pulled_arms_flat, rewards):
+            if self.change_detection[pulled_arm].update(reward):
+                self.detections[pulled_arm].append(self.t)
+                self.valid_rewards_per_arms[pulled_arm] = []
+                self.change_detection[pulled_arm].reset()
+            self.update_observations(pulled_arm, reward)
+            self.empirical_means[pulled_arm] = np.mean(self.valid_rewards_per_arms[pulled_arm])
+        total_valid_samples = sum([len(x) for x in self.valid_rewards_per_arm])
 
-    for a in range(self.n_arms):
-      n_samples=len(self.valid_rewards_per_arm[a])
-      self.confidence[a]=(2*np.log(total_valid_samples)/n_samples)**0.5 if n_samples>0 else np.inf
+        for a in range(self.n_arms):
+            n_samples = len(self.valid_rewards_per_arm[a])
+            self.confidence[a] = (2 * np.log(total_valid_samples) / n_samples) ** 0.5 if n_samples > 0 else np.inf
 
-  def update_observations(self, pulled_arm, reward):
-    self.rewards_per_arm[pulled_arm].append(reward)
-    self.valid_rewards_per_arm[pulled_arm].append(reward)
-    self.collected_rewards=np.append(self.collected_rewards, reward)
+    def update_observations(self, pulled_arm, reward):
+        self.rewards_per_arm[pulled_arm].append(reward)
+        self.valid_rewards_per_arm[pulled_arm].append(reward)
+        self.collected_rewards = np.append(self.collected_rewards, reward)
